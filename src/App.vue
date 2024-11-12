6e5de61fa67ef25171b3a8c30ce401031b666895
ravin yadav <ravinyadav973@gmail.com>
2024-11-12 22:13:55 +0530

    staring my vue js learing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
 <template>
-  <div>
-    <p v-if="showMessage" >{{ message }}</p>
-    <button @click="showMessage = !showMessage">Show/Hide</button>
-  </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      message: "I Start Learning Vue.js",
-      showMessage:true,
-    }
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-};
-</script>
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(Quasar, quasarUserOptions).mount('#app')
+createApp(App).mount('#app')
